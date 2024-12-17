9b17199a445ed62155c82fb29516a85b736160a8
Ram200231 <121919685+Ram200231@users.noreply.github.com>
2024-12-17 23:23:44 +0530

    Spalsh Screen kic start

--- FoodReviewApp/src/App.js
+++ FoodReviewApp/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './Assets/css/splash.css';
+import SplashScreen1 from './Assets/images/Splashscreen/screen_one_bg.png'
+import { PiChefHatDuotone } from "react-icons/pi";
+import { COLORS } from './Assets/constants/Color';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with nice
-        </a>
-      </header>
+    <div className='splash-container'>
+     <div className='splash-skip-btn'>
+      <span className='splash-skip-btn-txt'>Skip</span>
+     </div>
+     <div className='splash-top-container'>
+      <img src={SplashScreen1} className='splash-cover-img' alt="" />
+     </div>
+     <div className='splash-bottom-container'>
+      <div className='splash-icon-con'>
+      <PiChefHatDuotone size={35} color={COLORS.primary} />
+      </div>
+     </div>
     </div>
   );
 }
---
+++ FoodReviewApp/src/Assets/constants/Color.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+    primary: '#FF8527',
+    secondary: '#fff',
+    warning: '#e74c3c',
+    white: '#ffffff',
+    black: '#000000',
+    gray: '#95a5a6'
+  };
